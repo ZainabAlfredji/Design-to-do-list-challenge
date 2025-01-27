f20d16fb58cd22ac878e7e65d0914eedcdb7a794
Zainab Alfredji <zainab.alfredji@adbsafegate.com>
2025-01-27 16:00:40 +0100

    Add to do list functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'todo-app';
+  todos: string[] = [];
+  newTodo: string = '';
+
+  addTodo() {
+    if (this.newTodo.trim()) {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
 }
